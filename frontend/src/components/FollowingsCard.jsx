450d373088980dbc61a07b3de44a2c45a78b006c
fidaa-alshaikh <fidaa.alshaikh@gmail.com>
2021-05-27 22:23:12 +0300

    clean the code

--- frontend/src/components/FollowingsCard.jsx
+++ frontend/src/components/FollowingsCard.jsx
@@ -6,27 +6,22 @@ export default function FollowingsCard(props) {
 <div>
     
 
-                                    <div className="list-group-item d-flex align-items-center">
-                                        <img src={props.userImage} alt="" width="50px" className="rounded-sm ml-n2" />
-                                        <div className="flex-fill pl-3 pr-3">
-                                        <div className="text-dark font-weight-600">{props.name}</div>
+     <div className="list-group-item d-flex align-items-center">
+         <img src={props.userImage} alt="" width="50px" className="rounded-sm ml-n2" />
+         <div className="flex-fill pl-3 pr-3">
+         <div className="text-dark font-weight-600">{props.name}</div>
                                         
-                                            <div></div>
-                                            <a onClick={() => history.push(`/oneUser/${props._id}`)} href="" >@{props.userName}</a>
-                                        </div>
-                                        {props.notShow?
-                                        <></>
-                                        
-                                        :
-                                        <button type="button" className="btn btn-sm btn-danger pull-right" onClick={()=> props.unfollow(props._id)}><i className="fa fa-close-round"></i> Unfollow</button>}
+        <div></div>
+        <a onClick={() => history.push(`/oneUser/${props._id}`)} href="" >@{props.userName}</a>
+        </div>
+        {props.notShow?
+        <></>                        
+        :
+        <button type="button" className="btn btn-sm btn-danger pull-right" onClick={()=> props.unfollow(props._id)}><i className="fa fa-close-round"></i> Unfollow</button>}
 
-                                    </div>
+         </div>
 
-
-                                </div>
-                                
-
-
+        </div>
 
     )
 }
