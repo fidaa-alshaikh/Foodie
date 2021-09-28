cf67502d194b88f9df3cb46d643a27f976cba304
fidaa-alshaikh <fidaa.alshaikh@gmail.com>
2021-09-28 20:56:33 +0300

    remove footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,18 +4,7 @@ import React from 'react'
 export default function Footer() {
     return (
 
-<footer id="footer" className="footer-1">
-
-<div className="footer-copyright">
-<div className="container">
-<div className="row">
-<div className="col-md-12 text-center">
-<p>Copyright Fidaa Alshaikh© 2021. All rights reserved.</p>
-</div>
-</div>
-</div>
-</div>
-</footer>
+<></>
 
     )
 }
